2f88359467396fac40759bb10354bc3f6abc66fe
ktabassum1809 <ktabassum1809@gmail.com>
2025-01-30 14:01:46 +0100

    created userSChema and added CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/userRouter.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,10 +12,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Testing the server endpoint
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+
+// Connect to MongoDB
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed");
+    process.exit(1);  // Exit the process with a failure code
+  }
+}
+
+connectDB();
+
+
+
+app.use("/data", router);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
