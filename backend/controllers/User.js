cab62bce6b673a4449f00dcdf4f02b419e269e01
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-02-01 18:13:18 +0100

    fixed some formatting issues, and missing try,catch

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,7 +1,5 @@
-
 import User from "../models/userSchema.js";
 import jwt from "jsonwebtoken";
-
 
 export const createUsers = async (req, res) => {
   try {
@@ -11,12 +9,13 @@ export const createUsers = async (req, res) => {
         message: "Name, email, and password are required.",
       });
     }
-  const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
-        return res.status(400).json({
-            message: "User with this email already exists.please login as you are already a registered user",
-        });
-        }
+      return res.status(400).json({
+        message:
+          "User with this email already exists.please login as you are already a registered user",
+      });
+    }
     // Create the user
     const user = await User.create({
       name,
@@ -27,7 +26,11 @@ export const createUsers = async (req, res) => {
       address,
     });
 
-    const token = jwt.sign({userId: user._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: "1h"});
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
 
     // Respond with success
     res.status(201).json({
@@ -50,51 +53,52 @@ export const createUsers = async (req, res) => {
 };
 
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
- if (!email || !password) {
-   return res.status(400).json({
-     message: "Email and password are required.",
-   });
- }
- try{
-    const validateUser = await  User.findOne({ email });
-    if(!validateUser){
-        return res.status(404).json({
-            message: "User not found . Please sign up to use our services",
-        });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "Email and password are required.",
+    });
+  }
+
+  try {
+    const validateUser = await User.findOne({ email });
+    if (!validateUser) {
+      return res.status(404).json({
+        message: "User not found . Please sign up to use our services",
+      });
     }
     const validPassword = await validateUser.comparePassword(password);
     if (!validPassword) {
-        return res.status(401).json({
-          message: "Invalid credentials",
-        });
-      }
-        const token = jwt.sign({userId: validateUser._id, email: validateUser.email}, process.env.JWT_SECRET, {expiresIn: "1h"});
-       res.cookie("token", token, {httpOnly: true, maxAge: 3600000});
-      return res.status(200).json({
-        message: "Login successful",
-        data: {
-            _id: validateUser._id,
-            name: validateUser.name,
-            email: validateUser.email,
-            role: validateUser.role,
-          },
-        token: token,
+      return res.status(401).json({
+        message: "Invalid credentials",
       });
     }
+    const token = jwt.sign(
+      { userId: validateUser._id, email: validateUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    res.cookie("token", token, { httpOnly: true, maxAge: 3600000 });
 
     return res.status(200).json({
       message: "Login successful",
-      data: validateUser,
+      data: {
+        _id: validateUser._id,
+        name: validateUser.name,
+        email: validateUser.email,
+        role: validateUser.role,
+      },
+      token: token,
     });
   } catch (err) {
     res.status(500).json({
-        message: "You are not a registered user please sign up to use our services",
-        error: err.message,
+      message:
+        "You are not a registered user please sign up to use our services",
+      error: err.message,
     });
   }
 };
-
 
 export const getUsers = async (req, res) => {
   try {
@@ -106,13 +110,12 @@ export const getUsers = async (req, res) => {
     }
     res.status(200).json({
       message: "All users fetched successfully",
-      data: users
+      data: users,
     });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json({
       message: "An error occurred while fetching users",
-      error: err.message
+      error: err.message,
     });
   }
 };
@@ -127,36 +130,37 @@ export const getUserById = async (req, res) => {
     }
     res.status(200).json({
       message: "User fetched successfully",
-      data: user
+      data: user,
     });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json({
       message: "An error occurred while fetching user",
-      error: err.message
+      error: err.message,
     });
   }
 };
 
 export const updateUser = async (req, res) => {
   try {
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
     if (!updatedUser) {
       return res.status(404).json({
         message: "User not found",
       });
     }
     res.status(200).json({
-      message: "User updated successfully", data: updatedUser
+      message: "User updated successfully",
+      data: updatedUser,
     });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json({
       message: "An error occurred while updating user",
-      error: err.message
+      error: err.message,
     });
   }
-}
+};
 
 export const deleteUser = async (req, res) => {
   try {
@@ -164,39 +168,30 @@ export const deleteUser = async (req, res) => {
     if (!deletedUser) {
       return res.status(404).json({
         message: "User not found",
-
       });
     }
     res.status(200).json({
-      message: "User deleted successfully", data: deletedUser
+      message: "User deleted successfully",
+      data: deletedUser,
     });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json({
       message: "An error occurred while deleting user",
-      error: err.message
+      error: err.message,
     });
   }
-}
+};
 
 export const signOut = async (req, res) => {
-    try{
-        res.clearCookie("token");
-        res.status(200).json({
-            message: "Sign out successfully",
-        });
-    }
-    catch(err){
-        res.status(500).json({
-            message: "An error occurred while signing out",
-            error: err.message
-        });
-    }
-}
-
-
-
-
-
-
-
+  try {
+    res.clearCookie("token");
+    res.status(200).json({
+      message: "Sign out successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "An error occurred while signing out",
+      error: err.message,
+    });
+  }
+};
