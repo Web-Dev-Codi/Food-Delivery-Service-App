cc91ba19164f9dc20c92cb994ff63f7c061c6f45
ktabassum1809 <ktabassum1809@gmail.com>
2025-02-05 14:59:58 +0100

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import ListRestaurant from "./components/ListRestaurant";
 import AddRestaurant from  "./components/AddRestaurant";
 import SingleRestaurant from "./components/SingleRestaurant";
 import AddMenu from "./components/AddMenu";
+import AddReview from "./components/AddReview";
+import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/LoginForm";
 
 
 
@@ -11,10 +14,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AddRestaurant/>} />
+        <Route path="/" element={<SignupForm/>} />
+        <Route path="/login" element={<LoginForm/>} />
         <Route path="/restaurants" element={<ListRestaurant />} />
-      
+        <Route path="/restaurants/add" element={<AddRestaurant />} />
         <Route path="/restaurants/:id" element={<SingleRestaurant />} />
+        <Route path="/restaurants/:id/reviews" element={<AddReview />} />
         <Route path="/menu" element={<AddMenu />} />
       </Routes>
     </Router>
