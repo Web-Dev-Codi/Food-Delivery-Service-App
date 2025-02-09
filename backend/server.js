78cb4e02cb6f322c5e2758fef3fab055ed1cbf75
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-02-09 20:41:57 +0100

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./utils/db.js";
 import router from "./routes/routes.js";
 import dotenv from "dotenv";
+dotenv.config();
 import restaurantRouter from "./routes/restaurantRoute.js";
 import cors from "cors";
 import menuRouter from "./routes/menuRouter.js";
@@ -9,26 +10,23 @@ import { seedData } from "./controllers/Menu.js";
 import userRouter from "./routes/userRouter.js";
 import cartRouter from "./routes/cartRoutes.js";
 
-dotenv.config();
+connectDB();
 
 const app = express();
 
-
 app.use(
-  cors({
-    origin: "http://localhost:5173", // Replace with your front-end URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  }),
-)
+	cors({
+		origin: "http://localhost:5173", // Replace with your front-end URL
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB();
-app.get('/seed', (req, res) => {
-  seedData(req, res);
+app.get("/seed", (req, res) => {
+	seedData(req, res);
 });
-
 
 app.use("/", router);
 app.use("/data", userRouter);
@@ -37,7 +35,7 @@ app.use("/api", cartRouter);
 // Not sure why we have both 2 /data endpoints
 // app.use("/data", router);
 app.use("/api", restaurantRouter);
-app.use("/food",menuRouter);
+app.use("/food", menuRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
