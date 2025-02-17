e896367c62af971836c02eaf273d3155cff7ae29
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-02-17 16:21:55 +0100

    implement singleRestaurant.jsx-styling img lay-out

--- frontend/src/components/SingleRestaurant.jsx
+++ frontend/src/components/SingleRestaurant.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import Logo from "../assets/images/icescream.jpg";
 
 function SingleRestaurant() {
   const { id } = useParams();
@@ -66,58 +67,78 @@ function SingleRestaurant() {
 
   return (
     <>
-      <select
-        className="w-full bg-gray-100 text-gray-800 px-4 py-2 rounded-md mb-4"
-        value={menuCategory}
-        onChange={(e) => setMenuCategory(e.target.value)}
-      >
-        <option value="Select">All Categories</option>
-        <option value="Main Course">Main Course</option>
-        <option value="Dessert">Dessert</option>
-        <option value="Beverages">Beverages</option>
-      </select>
-
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md mx-auto">
+      <div className="bg-neutral-900 p-8 rounded-lg shadow-lg w-full max-w-md mx-auto">
         {/* Restaurant Image */}
-        <div>
+        <div className="relative">
           <img
             src={restaurant.images[0]}
             alt={restaurant.name}
             className="w-full h-64 object-cover rounded-lg mb-6"
           />
 
-          <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">
-            {restaurant.name}
-          </h2>
-          <p className="text-gray-700 mb-6">{restaurant.location}</p>
-          <p className="text-gray-700 mb-6">{restaurant.contact}</p>
-        </div>
+          {/* Restaurant Info */}
+          <div className="absolute left-4 bottom-[-2] transform -translate-y-1/2 bg-neutral-800/20 backdrop-blur-md rounded-lg p-6 shadow-lg flex items-center gap-4 z-50 ">
+            {/* Restaurant Logo */}
+            <img
+              className="w-16 h-16 object-cover rounded-full"
+              src={Logo}
+              alt="Logo"
+            />
 
-        <div className="text-center">
+            {/* Restaurant name */}
+            <div>
+              <h2 className="text-2xl font-bold text-neutral-200">
+                {restaurant.name}
+              </h2>
+              <p className="text-gray-200">{restaurant.location}</p>
+              <p className="text-gray-200">{restaurant.contact}</p>
+            </div>
+          </div>
+        </div>
+        <select
+          className="w-full bg-gray-100 text-gray-800 px-4 py-2 rounded-md mt-16 mb-4"
+          value={menuCategory}
+          onChange={(e) => setMenuCategory(e.target.value)}
+        >
+          <option value="Select">All Categories</option>
+          <option value="Main Course">Main Course</option>
+          <option value="Dessert">Dessert</option>
+          <option value="Beverages">Beverages</option>
+        </select>
+
+        <div className="text-center bg-gradient-to-r from-[#e62b2b] via-[#22193f] to-[#3e1a5d] p-4 rounded-lg shadow-lg">
           {filteredMenus.length > 0 ? (
             filteredMenus.map((menu) => (
-              <div key={menu._id} className="mb-4">
-                <p className="text-gray-700 mb-2">{menu.name}</p>
+              <div
+                key={menu._id}
+                className="flex items-center gap-6 mb-6 bg-neutral-800/50 backdrop-blur-md p-3 rounded-lg shadow-md border border-neutral-500"
+              >
+                <div className="flex-1 text-left">
+                  <p className="text-neutral-300 text-lg font-bold mb-2">
+                    {menu.name}
+                  </p>
+                  <p className="text-gray-200 text-lg font-medium mb-4">
+                    €{menu.price}
+                  </p>
+                  <button
+                    type="button"
+                    onClick={() => handleClick(menu._id)}
+                    className="bg-transparent text-green-400 px-4 py-2 rounded-md hover:text-neutral-300"
+                  >
+                    Show Details
+                  </button>
+                </div>
+
+                {/* Menu Image - Right Side */}
                 <img
                   src={menu.imageUrl}
                   alt={menu.name}
-                  className="w-full h-64 object-cover mb-4"
+                  className="w-48 h-32 object-cover rounded-lg shadow-md"
                 />
-                <p className="text-gray-700 mb-2">${menu.price}</p>
-
-                <button
-                  type="button"
-                  onClick={() => handleClick(menu._id)}
-                  className="w-full bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400"
-                >
-                  Show Details
-                </button>
               </div>
             ))
           ) : (
-            <div className="text-red-600">
-              No menu available for this category
-            </div>
+            <p className="text-gray-200">No menus available.</p>
           )}
         </div>
 
