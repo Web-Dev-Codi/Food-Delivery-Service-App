46050c749bb2440c6b231e07ccb9eee1e018b402
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-02-17 11:54:31 +0100

    updated Hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/views/Header";
 import Footer from "./components/views/Footer";
-import Hero from "./components/Hero"; // Import the Hero component
 
 function App() {
 	return (
 		<React.Fragment>
 			<Header />
 			<main className="bg-gradient-to-b from-[#0B1225] via-[#382677] to-[#050913] flex flex-col backdrop-blur-xl">
-				<Hero/>
 				<Outlet />
 			</main>
-
 			<Footer />
 		</React.Fragment>
 	);
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,27 +1,49 @@
-export default function HeroSection() {
-  return (
-    <section className="flex items-center justify-between p-10 bg-[rgb(25,25,112)] min-h-screen">
-      {/* Left Side */}
-      <div className="max-w-xl">
-        <h1 className="text-4xl font-bold text-[#ffbb28]">
-          Bringing flavours to your doorstep!
-        </h1>
-        <p className="mt-10 text-[#f0db67]">
-          At Four Flavors Express, we bring the world of flavors straight to
-          your doorstep fresh, fast, and full of taste. Whether you’re craving
-          comfort food, exotic cuisine, or a quick bite, we deliver with speed
-          and care. Your favorite meals, just a tap away!
-        </p>
-      </div>
+import Hero2 from "../assets/images/food2.avif";
 
-      {/* Right Side */}
-      <div>
-        <img
-          src="src/assets/images/food delivary image.avif"
-          alt="Delicious food"
-          className="w-[800px] rounded-xl shadow-lg"
-        />
-      </div>
-    </section>
-  );
+export default function HeroSection() {
+	return (
+		<div className="w-full">
+			<section className="bg-transparent text-white w-3/4 mx-auto">
+				<div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-center">
+					<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+						<h1 className="text-5xl font-bold leading-none sm:text-6xl">
+							Healthy food <br />
+							<span className="dark:text-[#D84418] mr-4">
+								right
+							</span>
+							to your door fast!
+						</h1>
+						<p className="mt-6 mb-8 text-lg sm:mb-12">
+							lorem ipsum dolor sit amet, consectetur adipiscing
+							elit. Dictum aliquam porta in condimentum ac
+							integer.
+							<br className="hidden md:inline lg:hidden" />
+							turpis pulvinar, est scelerisque ligula sem
+						</p>
+						<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+							<a
+								rel="noopener noreferrer"
+								href="#"
+								className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">
+								Order Now
+							</a>
+							<a
+								rel="noopener noreferrer"
+								href="#"
+								className="px-8 py-3 text-lg bg-[#D84418] font-semibold border rounded border-[#D84418]">
+								Login
+							</a>
+						</div>
+					</div>
+					<div className="flex items-center justify-center">
+						<img
+							src={Hero2}
+							alt=""
+							className="object-contain w-full h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+						/>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
 }
