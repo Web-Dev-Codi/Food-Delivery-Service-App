955ea90ec398c92f0e788951f758e18fee8b539f
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-02-27 14:51:34 +0100

    update AddRestaurant.jsx

--- frontend/src/components/AddRestaurant.jsx
+++ frontend/src/components/AddRestaurant.jsx
@@ -92,8 +92,8 @@ function AddRestaurant() {
   };
 
   return (
-    <div className="flex items-center justify-center w-full min-h-screen">
-      <div className="bg-neutral-800/30 backdrop-blur p-8 rounded-lg shadow-lg w-full max-w-md">
+    <div className="flex items-center justify-center w-full md:mt-28 ">
+      <div className="bg-red-800/30 backdrop-blur p-8 rounded-lg shadow-lg w-full max-w-md">
         <h1 className="text-2xl font-bold mb-6 text-center text-white">
           Add Restaurant
         </h1>
